bb718f51289649004ff5f6b401f9bd4e2b99b461
Nicholas Otieno <nich.otieno@gmail.com>
2023-08-10 21:48:54 +0300

    update

--- app/src/main/java/com/example/foundit/Onboarding.kt
+++ app/src/main/java/com/example/foundit/Onboarding.kt
@@ -2,8 +2,13 @@ package com.example.foundit
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,23 +17,44 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Preview(showBackground = true)
 @Composable
-fun OnboardingScreen(){
+fun OnboardingScreen() {
     Column(
-        modifier = Modifier.fillMaxSize().padding(start = 16.dp, end = 16.dp), horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, end = 16.dp, bottom = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
 
-    ){
+    ) {
         Image(
             painter = painterResource(id = R.drawable.search),
             contentDescription = "Search",
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+
         )
-        Text("Welcome to FoundIt!", style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 24.sp))
-        Text("FoundIt - Reuniting you with your lost  items")
+        Spacer(Modifier.height(64.dp))
+        Text(
+            "Welcome to FoundIt!",
+            style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 36.sp)
+        )
+        Spacer(Modifier.height(32.dp))
+        Text("Reuniting you with your lost  items",style = TextStyle(fontWeight = FontWeight.W400, fontSize = 24.sp, textAlign = TextAlign.Center))
+        Spacer(Modifier.height(64.dp))
+        Button(onClick = { /*TODO*/ },modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp), shape = RoundedCornerShape(8.dp)
+        ) {
+            Text("Start",style = TextStyle(fontWeight = FontWeight.Bold))
+
+        }
     }
 
 }
