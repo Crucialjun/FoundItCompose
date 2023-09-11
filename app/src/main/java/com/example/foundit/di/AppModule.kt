c98b6efdb8084d694fb8507f566ef04039376fda
nich.otieno <nicholas.otieno@revca.tech>
2023-09-11 11:08:37 +0300

    updates

---
+++ app/src/main/java/com/example/foundit/di/AppModule.kt
@@ -0,0 +1,38 @@
+package com.example.foundit.di
+
+import android.content.Context
+import com.example.foundit.features.auth.data.datasources.AuthDataSource
+import com.example.foundit.features.auth.data.datasources.AuthDataSourceImp
+import com.example.foundit.features.auth.data.repository.AuthRepository
+import com.example.foundit.features.auth.data.repository.AuthRepositoryImp
+import com.google.android.gms.auth.api.identity.SignInClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+    @Provides
+    @Singleton
+    fun provideAuthRepository(
+        authDataSource: AuthDataSource
+    ): AuthRepository {
+        return AuthRepositoryImp(authDataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthDataSource(
+        @ApplicationContext
+        context: Context,
+        oneTapClient : SignInClient
+    ): AuthDataSource {
+        return AuthDataSourceImp(
+            context,oneTapClient
+        )
+    }
+}
