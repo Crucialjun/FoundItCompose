dcbfa51be8d1f169199cb96fb70a6c592e1e91cc
nicholas.otieno <nicholas.otieno@revca.tech>
2023-10-16 10:54:23 +0300

    add db and auth services

--- app/src/main/java/com/example/foundit/features/profile_setup/views/ProfileSetupViews.kt
+++ app/src/main/java/com/example/foundit/features/profile_setup/views/ProfileSetupViews.kt
@@ -1,17 +1,13 @@
 package com.example.foundit.features.profile_setup.views
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.example.foundit.features.auth.viewmodels.LoginViewModel
-
 
 
 @Composable
@@ -24,9 +20,9 @@ fun ProfileSetupView(
     val viewmodel: LoginViewModel = hiltViewModel(backStackEntry)
     Column {
            Text("Fill in your profile")
-            Text(text = "The user is ${viewmodel.appUser.value.name}")
+
             AsyncImage(
-                model = "https://example.com/image.jpg",
+                model = viewmodel.appUser.value.profilePicUrl,
                 contentDescription = null,
             )
 
